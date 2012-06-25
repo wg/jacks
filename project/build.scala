ca85ec9e83d97cf04edbcfebdeb57e7020d64bd1
Will <will@glozer.net>
2012-06-25 19:44:26 +0900

    upgrade jackson to 2.0.2

--- project/build.scala
+++ project/build.scala
@@ -4,13 +4,13 @@ import Keys._
 object JacksBuild extends Build {
   val buildSettings = Project.defaultSettings ++ Seq(
     name         := "jacks",
-    version      := "2.0.1",
+    version      := "2.0.2",
     organization := "com.lambdaworks",
     scalaVersion := "2.9.2",
 
     libraryDependencies <+= scalaVersion("org.scala-lang" % "scalap" % _),
     libraryDependencies ++= Seq(
-      "com.fasterxml.jackson.core" % "jackson-databind" % "2.0.1",
+      "com.fasterxml.jackson.core" % "jackson-databind" % "2.0.2",
       "org.scalatest" %% "scalatest" % "1.6.1" % "test",
       "org.scalacheck" %% "scalacheck" % "1.9" % "test",
       "junit" % "junit" % "4.10" % "test"
