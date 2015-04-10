cbcf7ab076a3979e7ce4b05f1ebb78591e109023
rampalli-github <rampalli.github@gmail.com>
2015-04-10 11:30:42 +0530

    upgrade sbt, scala, scalate, scalacheck, jackson

--- project/build.scala
+++ project/build.scala
@@ -4,17 +4,17 @@ import Keys._
 object JacksBuild extends Build {
   val buildSettings = Project.defaultSettings ++ Seq(
     name         := "jacks",
-    version      := "2.3.3",
+    version      := "2.3.4",
     organization := "com.lambdaworks",
-    scalaVersion := "2.11.0",
+    scalaVersion := "2.11.6",
 
-    crossScalaVersions := Seq("2.11.0", "2.10.4"),
+    crossScalaVersions := Seq("2.11.6", "2.10.5"),
 
     libraryDependencies <+= scalaVersion("org.scala-lang" % "scalap" % _),
     libraryDependencies ++= Seq(
-      "com.fasterxml.jackson.core" % "jackson-databind" % "2.3.3",
-      "org.scalatest" %% "scalatest" % "2.1.3" % "test",
-      "org.scalacheck" %% "scalacheck" % "1.11.3" % "test"
+      "com.fasterxml.jackson.core" % "jackson-databind" % "2.4.4",
+      "org.scalatest" %% "scalatest" % "2.2.4" % "test",
+      "org.scalacheck" %% "scalacheck" % "1.12.2" % "test"
     ),
 
     scalacOptions ++= Seq( "-language:_", "-unchecked", "-optimize"),
@@ -29,31 +29,31 @@ object JacksBuild extends Build {
       }
     },
 
-    pomIncludeRepository := { _ => false },
-    pomExtra             := (
-      <url>http://github.com/wg/jacks</url>
-
-      <scm>
-        <connection>scm:git:git://github.com/wg/jacks.git</connection>
-        <developerConnection>scm:git:git://github.com/wg/jacks.git</developerConnection>
-        <url>http://github.com/wg/jacks</url>
-      </scm>
-
-      <licenses>
-        <license>
-          <name>Apache License, Version 2.0</name>
-          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-          <distribution>repo</distribution>
-        </license>
-      </licenses>
-
-      <developers>
-        <developer>
-          <id>will</id>
-          <name>Will Glozer</name>
-        </developer>
-      </developers>
-    )
+    pomIncludeRepository := { _ => false }//,
+//    pomExtra             := (
+//      <url>http://github.com/wg/jacks</url>
+//
+//      <scm>
+//        <connection>scm:git:git://github.com/wg/jacks.git</connection>
+//        <developerConnection>scm:git:git://github.com/wg/jacks.git</developerConnection>
+//        <url>http://github.com/wg/jacks</url>
+//      </scm>
+//
+//      <licenses>
+//        <license>
+//          <name>Apache License, Version 2.0</name>
+//          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+//          <distribution>repo</distribution>
+//        </license>
+//      </licenses>
+//
+//      <developers>
+//        <developer>
+//          <id>will</id>
+//          <name>Will Glozer</name>
+//        </developer>
+//      </developers>
+//    )
   )
 
   val jacks = Project(id = "jacks", base = file("."), settings = buildSettings)
